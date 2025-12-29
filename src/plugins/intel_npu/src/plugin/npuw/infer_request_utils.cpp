d9636fa0f91993dbafa1909e997f4b9a5ad88df5
Sergey Shumihin <sergey.shumihin@intel.com>
2025-12-29 18:26:20 +0000

    Add infer request logic to kokoro pipeline

--- src/plugins/intel_npu/src/plugin/npuw/infer_request_utils.cpp
+++ src/plugins/intel_npu/src/plugin/npuw/infer_request_utils.cpp
@@ -158,3 +158,18 @@ std::optional<ov::Output<const ov::Node>> ov::npuw::util::find_port_by_name(
     }
     return std::make_optional(*it);
 }
+
+std::optional<ov::Output<const ov::Node>> ov::npuw::util::find_port_by_names(
+    const std::vector<ov::Output<const ov::Node>>& ports,
+    const std::unordered_set<std::string>& names) {
+    for (const auto& port: ports) {
+        const auto& port_names = port.get_names();
+        for (const auto& port_name: port_names) {
+            if (names.count(port_name)) {
+                return std::make_optional(port);
+            }
+        }
+    }
+    return std::nullopt;
+}
+
--- src/plugins/intel_npu/src/plugin/npuw/infer_request_utils.hpp
+++ src/plugins/intel_npu/src/plugin/npuw/infer_request_utils.hpp
@@ -36,6 +36,12 @@ void copy_tensor_by_dim(ov::SoPtr<ov::ITensor> src_tensor,
 std::optional<ov::Output<const ov::Node>> find_port_by_name(const std::vector<ov::Output<const ov::Node>>& ports,
                                                             const std::string& name);
 
+/**
+ * @brief Searches for a port within a collection that matches any of the specified names.
+ */
+std::optional<ov::Output<const ov::Node>> find_port_by_names(const std::vector<ov::Output<const ov::Node>>& ports,
+                                                            const std::unordered_set<std::string>& names);
+
 }  // namespace util
 }  // namespace npuw
 }  // namespace ov
